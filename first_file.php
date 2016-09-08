f73beeca1a53f95d89864fb3b88e86167c2ec105
karokrii <karokrii@greeny.cs.tlu.ee>
2016-09-08 13:12:13 +0300

    added my first line of code

---
+++ first_file.php
@@ -0,0 +1,11 @@
+<?php
+   $firstName = "Karolin";
+   $lastName = "Kriiska";
+   //echo $firstName;
+   //echo $lastName;
+   
+   // Kahe stringi liitmiseks kasutan punkti
+   echo $firstName." ".$lastName;
+   
+   
+?>
